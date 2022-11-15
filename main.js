59f6cf8d95a70cf6e4a76018f737dc5227612360
frontendsara <sara.karlsson1991@hotmail.com>
2022-11-15 09:41:41 +0100

    add first files

---
+++ main.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", main);
+
+function main(){
+    setTimeout(changeBackgroundColor, 4000);
+}
+//Start på programmet
+
+
+
+function changeBackgroundColor() {
+    document.body.style.background = "lightblue"
+}
