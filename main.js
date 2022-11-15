7173a7f7a0a8ad296bfeadb2208951330b38f9fe
frontendsara <sara.karlsson1991@hotmail.com>
2022-11-15 10:59:54 +0100

    added random backgroundcolors

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
 window.addEventListener("DOMContentLoaded", main);
 
-function main(){
-    setTimeout(changeBackgroundColor, 4000);
-}
-//Start på programmet
+let timeoutRef;
 
+function main(){
+    //Start på programmet
+    timeoutRef = setTimeout(changeBackgroundColor, 2000);
+    addEventListeners();
+}
 
 
 function changeBackgroundColor() {
-    document.body.style.background = "lightblue"
+    document.body.style.background = getRandomColor();
+}
+
+function addEventListeners(){
+    const cancelTimeoutbutton = document.getElementById("cancel")
+    cancelTimeoutbutton.addEventListener("click", cancelBackgroundChange);
+
+    const startIntervalButton = document.getElementById("start-interval");
+    startIntervalButton.addEventListener("click", startBackgroundChangeInterval)
+}
+
+function cancelBackgroundChange(){
+    clearTimeout(timeoutRef);
+    //stänger av 
+}
+
+function startBackgroundChangeInterval(){
+    setInterval(changeBackgroundColor, 1000);
+}
+
+function getRandomColor(){
+    const value = Math.random();
+
+    if (value < 0.2) {
+        return "blue";
+    } else if (value < 0.4) {
+        return "red";
+    } else if ( value < 0.6) {
+        return "green";
+    } else if ( value < 0.8) {
+        return "grey";
+    } else {
+        return "pink";
+    }
 }
